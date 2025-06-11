850bd485ea5c1c944dd2ebb5d3393deee286f528
Joshua Hughes <kivhift@gmail.com>
2025-06-11 10:14:17 -0400

    Clean up test bench relative to input/output size

--- getting-started/proj/01/test/sw-to-led-tb.vhdl
+++ getting-started/proj/01/test/sw-to-led-tb.vhdl
@@ -10,8 +10,7 @@ entity switches_to_leds_tb is
 end;
 
 architecture tb of switches_to_leds_tb is
-    signal input: std_logic_vector(0 to 3);
-    signal output: std_logic_vector(0 to 3);
+    signal input, output: std_logic_vector(0 to 3);
 begin
 
 DUT:
@@ -30,13 +29,12 @@ DUT:
     begin
         test_runner_setup(runner, runner_cfg);
 
-        for i in 0 to 15 loop
-            input <= std_logic_vector(to_unsigned(natural(i), 4));
+        for i in 0 to (2 ** input'length) - 1 loop
+            input <= std_logic_vector(to_unsigned(natural(i), input'length));
+            wait for ns;
             check_equal(output, input);
         end loop;
 
-        -- vcom complains if there's no wait statement within the process.
-        wait for ns;
         test_runner_cleanup(runner);
     end process;
 end;
