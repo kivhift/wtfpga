8c317b5034ff399c965bc195c60db39d4ee279d2
Joshua Hughes <kivhift@gmail.com>
2025-12-19 17:21:12 -0500

    Lab 1: Switch to CPU_RESETN for reset
    This commit uses the CPU reset for reset and now uses the center button for
    enabling the PWM for the RGB LEDs. Names of ports have also been changed to
    make them clearer/more succinct.

--- labs/src/lab-01.vhdl
+++ labs/src/lab-01.vhdl
@@ -4,22 +4,22 @@ library ieee;
 
 entity lab_01 is
     generic (
+        -- Toggle every half second.
         BLINK_EVERY: positive := 50000000;
         -- PWM with 10% duty at 1kHz.
         PWM_PERIOD: positive := 100000;
         PWM_DUTY: natural := 10000
     );
     port (
-        clk: in std_logic;
-        reset: in std_logic;
-        btn_n, btn_s, btn_e, btn_w: in std_logic;
+        clk, reset_n: in std_logic;
+        btn_c, btn_n, btn_s, btn_e, btn_w: in std_logic;
         sw: in std_logic_vector(15 downto 0);
 
-        led: out std_logic_vector(15 downto 0);
+        leds: out std_logic_vector(15 downto 0);
         ssd_abcdefg: out std_logic_vector(6 downto 0);
         ssd_dp: out std_logic;
         ssd_digit_en: out std_logic_vector(7 downto 0);
-        led_r0, led_g0, led_b0, led_r1, led_g1, led_b1: out std_logic
+        rgb_leds: out std_logic_vector(5 downto 0)
     );
 end;
 
@@ -30,12 +30,12 @@ begin
 blink_pwm:
     entity work.pulse_width_modulation(rtl)
         generic map (
-            PERIOD => BLINK_EVERY * 2,
+            PERIOD => 2 * BLINK_EVERY,
             DUTY => BLINK_EVERY
         )
         port map (
             clk => clk,
-            rst => reset,
+            rst => not reset_n,
             en => '1',
             output => blink
         );
@@ -48,12 +48,12 @@ brightness_pwm:
         )
         port map (
             clk => clk,
-            rst => reset,
-            en => '1',
+            rst => not reset_n,
+            en => not btn_c,
             output => rgb_pwm
         );
 
-    led <=
+    leds <=
         16x"ffff" when btn_n else
         16x"5555" when btn_w else
         16x"aaaa" when btn_e else
@@ -64,10 +64,5 @@ brightness_pwm:
     ssd_digit_en <= (others => not (btn_s or blink));
 
     rgb_blink <= blink and rgb_pwm;
-    led_r0 <= rgb_blink;
-    led_g0 <= rgb_blink;
-    led_b0 <= rgb_blink;
-    led_r1 <= rgb_blink;
-    led_g1 <= rgb_blink;
-    led_b1 <= rgb_blink;
+    rgb_leds <= (others => rgb_blink);
 end;
