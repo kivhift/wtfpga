c36949388a0641c131884e37d7183098fdf8624a
Joshua Hughes <kivhift@gmail.com>
2026-06-06 18:51:10 -0400

    Update to GHDL v6.0.0 and VUnit v4.7.1
    This commit also tweaks the setup script to make shellcheck happy.

--- tools/setup-environment.sh
+++ tools/setup-environment.sh
@@ -4,10 +4,7 @@
 # VUnit using GHDL for simulation. Subdirectories will be created next to this
 # script to contain an install of GHDL built from source and a Python virtual
 # environment with VUnit installed. At the time of writing, the current stable
-# release versions are VUnit v4.7.0 and GHDL v5.1.1. Unfortunately, VUnit does
-# not work out of the box as a result of GHDL changing its version output to
-# indicate that it JITs in version 5. Thus, VUnit is patched to work with the
-# newer version.
+# release versions are VUnit v4.7.1 and GHDL v6.0.0.
 
 set -euo pipefail
 
@@ -63,18 +60,19 @@ while getopts ":h" opt; do
 done
 
 declare -ar needed_tools=(
-    dirname find git gnat make mkdir patch python3 realpath
+    dirname git gnat make mkdir python3 realpath
 )
 
-say "Checking for needed tools: ${needed_tools[@]}"
-type ${needed_tools[@]} > /dev/null 2>&1 \
-    || die "One or more of the needed tools are missing"
+say "Checking for needed tools: ${needed_tools[*]}"
+type "${needed_tools[@]}" > /dev/null 2>&1 ||
+    die "One or more of the needed tools are missing: ${needed_tools[*]}"
 
-declare -r script_dir="$(dirname $(realpath $0))"
+script_dir="$(dirname "$(realpath "$0")")"
+readonly script_dir
 say "Creating directory structure"
-mkdir -p $script_dir/{vunit,ghdl/{build,install}}
+mkdir -p "$script_dir"/{vunit,ghdl/{build,install}}
 
-declare -r ghdl_repo_uri=https://github.com/ghdl/ghdl.git ghdl_version=v5.1.1
+declare -r ghdl_repo_uri=https://github.com/ghdl/ghdl.git ghdl_version=v6.0.0
 declare -r ghdl_dir="$script_dir/ghdl"
 declare -r ghdl_install_dir="$ghdl_dir/install"
 
@@ -82,17 +80,17 @@ say "Changing to $ghdl_dir"
 cd "$ghdl_dir" || die "Could not change to $ghdl_dir"
 
 say "Cloning GHDL"
-git clone -b $ghdl_version $ghdl_repo_uri repo \
-    || die "Had trouble cloning GHDL repository"
+git clone -b $ghdl_version $ghdl_repo_uri repo ||
+    die "Had trouble cloning GHDL repository"
 
 say "Building GHDL"
 cd build || die "Could not change to build directory"
-../repo/configure --prefix="$ghdl_install_dir" \
-    || die "Could not configure GHDL"
+../repo/configure --prefix="$ghdl_install_dir" ||
+    die "Could not configure GHDL"
 make || die "Had trouble building GHDL"
 make install || die "Had trouble installing GHDL to $ghdl_install_dir"
 
-declare -r vunit_dir="$script_dir/vunit" vunit_version=4.7.0
+declare -r vunit_dir="$script_dir/vunit" vunit_version=4.7.1
 
 say "Changing to $vunit_dir"
 cd "$vunit_dir" || die "Could not change to $vunit_dir"
@@ -101,13 +99,11 @@ say "Setting up virtual environment"
 python3 -m venv venv || die "Had trouble setting up venv"
 
 say "Activating venv and installing vunit"
+# This file is created by the venv setup above, so silence shellcheck's
+# complaint about the file not being there.
+# shellcheck disable=SC1091
 source venv/bin/activate || die "Failed to activate venv"
 pip install vunit_hdl==$vunit_version || die "Failed to install vunit"
-
-declare -r vunit_patch="$script_dir/vunit-v4.7.0-ghdl-v5.patch"
-say "Patching vunit to work with GHDL v5"
-patch -p6 $(find venv -type f -name ghdl.py) $vunit_patch \
-    || die "Failed to patch vunit to work with GHDL"
 
 declare -r activation="$script_dir/activate-environment.sh"
 say "Creating activation script"
